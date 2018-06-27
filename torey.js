f3b6aed1e372c54f994bf8a16053e76268ccb48c
Toreytt15 <40604831+Toreytt15@users.noreply.github.com>
2018-06-27 10:24:39 -0500

    Add files via upload

---
+++ torey.js
@@ -0,0 +1,18 @@
+function conosolePrint(){
+   console.log("one two freddys coming for you");
+ }
+  function alertPrint(){
+   alert("three four better lock the door");
+ }
+  function documentPrint(){
+   document.write("runnn")
+ }
+function thesehands(){
+   console.log("sleeeep");
+ }
+  function totakeal(){
+   alert("you took a L");
+ }
+  function acookie(){
+   document.write("runnn")
+ }
